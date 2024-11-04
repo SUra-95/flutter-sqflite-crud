d84b85d7d73826279fe70384bda7f1bc5582b9b4
Suranjan De Silva <suranjandesilva1995@gmail.com>
2024-11-05 00:16:14 +0530

    change crud from sql into functions in sqflite

--- app/lib/database/todo_db.dart
+++ app/lib/database/todo_db.dart
@@ -1,11 +1,11 @@
 import 'package:app/database/database_service.dart';
 import 'package:app/model/todo.dart';
 import 'package:sqflite/sqflite.dart';
- 
- class TodoDB{
+
+class TodoDB {
   final tableName = 'todos';
 
-  Future <void> createTable(Database database) async {
+  Future<void> createTable(Database database) async {
     await database.execute('''
       CREATE TABLE IF NOT EXISTS $tableName (
         "id" INTEGER NOT NULL,
@@ -17,35 +17,68 @@ import 'package:sqflite/sqflite.dart';
     ''');
   }
 
-  Future <void> create ({required String title}) async {
+  // Future <void> create ({required String title}) async {
+  //   final database = await DatabaseService().database;
+
+  //    await database.rawInsert(
+  //     '''INSERT INTO $tableName (title, created_at) VALUES (?,?)''',
+  //     [title, DateTime.now().millisecondsSinceEpoch],
+  //   );
+  // }
+
+  Future<void> create({required String title}) async {
     final database = await DatabaseService().database;
 
-     await database.rawInsert(
-      '''INSERT INTO $tableName (title, created_at) VALUES (?,?)''',
-      [title, DateTime.now().millisecondsSinceEpoch],
+    await database.insert(
+      tableName,
+      {
+        'title': title,
+        'created_at': DateTime.now().millisecondsSinceEpoch,
+      },
     );
   }
 
-  Future <List<Todo>> fetchAll() async {
+  // Future <List<Todo>> fetchAll() async {
+  //   final database = await DatabaseService().database;
+  //   final todos = await database.rawQuery(
+  //     '''SELECT * FROM $tableName ORDER BY COALESCE(updated_at, created_at)''');
+  //     return todos.map((todo) => Todo.fromSqfliteDatabase(todo)).toList();
+  // }
+
+  Future<List<Todo>> fetchAll() async {
     final database = await DatabaseService().database;
-    final todos = await database.rawQuery(
-      '''SELECT * FROM $tableName ORDER BY COALESCE(updated_at, created_at)''');
-      return todos.map((todo) => Todo.fromSqfliteDatabase(todo)).toList();
+    final List<Map<String, dynamic>> todos = await database.query(
+      tableName,
+      // orderBy: 'COALESCE(updated_at, created_at)',
+    );
+    return todos.map((todo) => Todo.fromSqfliteDatabase(todo)).toList();
   }
 
-  Future <Todo> fetchById(int id) async {
+  // Future <Todo> fetchById(int id) async {
+  //   final database = await DatabaseService().database;
+  //   final todo = await database.rawQuery(
+  //     '''SELECT * FROM $tableName WHERE id = ?''', [id]);
+  //     return Todo.fromSqfliteDatabase(todo.first);
+  // }
+
+  Future<Todo> fetchById(int id) async {
     final database = await DatabaseService().database;
-    final todo = await database.rawQuery(
-      '''SELECT * FROM $tableName WHERE id = ?''', [id]);
-      return Todo.fromSqfliteDatabase(todo.first);
+    final List<Map<String, dynamic>> todos = await database.query(
+      tableName,
+      where: 'id = ?',
+      whereArgs: [id],
+    );
+
+    return Todo.fromSqfliteDatabase(todos.first);
   }
 
-  Future <void> update ({required int id, String? title}) async {
+  Future<void> update({required int id, String? title}) async {
     final database = await DatabaseService().database;
-     await database.update(tableName,
+    await database.update(
+      tableName,
       {
-        if (title != null) 'title' : title,
-        'updated_at' : DateTime.now().millisecondsSinceEpoch,
+        if (title != null) 'title': title,
+        'updated_at': DateTime.now().millisecondsSinceEpoch,
       },
       where: 'id = ?',
       // conflictAlgorithm: ConflictAlgorithm.rollback,
@@ -53,8 +86,17 @@ import 'package:sqflite/sqflite.dart';
     );
   }
 
-  Future <void> delete (int id) async {
-    final database = await DatabaseService().database;
-    await database.rawDelete('''DELETE FROM $tableName WHERE id = ?''', [id]);
-  }
- }
+  // Future<void> delete(int id) async {
+  //   final database = await DatabaseService().database;
+  //   await database.rawDelete('''DELETE FROM $tableName WHERE id = ?''', [id]);
+  // }
+  Future<void> delete(int id) async {
+  final database = await DatabaseService().database;
+  await database.delete(
+    tableName,
+    where: 'id = ?',
+    whereArgs: [id],
+  );
+}
+
+}
